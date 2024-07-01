3d33f0f48c8c99d45f63989f9dd44337a27aa37f
sb62dev <sb62dev@gmail.com>
2024-07-01 21:06:52 +0530

    CSS updations
    CSS updations

--- wp-content/themes/kingsguard/template-parts/trainingsection.php
+++ wp-content/themes/kingsguard/template-parts/trainingsection.php
@@ -1,11 +1,11 @@
-<section class="noLicense"> 
+<section class="noLicense py100"> 
     <div class="sm_container">
         <div class="noLicense_main">
             <div class="noLicense_txt">
-                <h2> Don't have a security guard license? </h2>
+                <h2 class="h2"> Don't have a security guard license? </h2>
             </div>
-            <div class="noLicense_btn">
-                <a href="https://ontariosecurityhub.ca/training/login/signup.php?source=KingsGuardSecurity" target="_blank"> Sign Up for Training </a>
+            <div class="noLicense_btn text-center">
+                <a class="btn-style" href="https://ontariosecurityhub.ca/training/login/signup.php?source=KingsGuardSecurity" target="_blank"> Sign Up for Training </a>
             </div>
         </div>  
     </div>  
