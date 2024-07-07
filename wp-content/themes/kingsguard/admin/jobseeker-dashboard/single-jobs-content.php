ddeba770081c1384e79859e36b917a8a513ced02
sb62dev <sb62dev@gmail.com>
2024-07-07 17:59:47 +0530

    Job page updations
    Job page updations

--- wp-content/themes/kingsguard/admin/jobseeker-dashboard/single-jobs-content.php
+++ wp-content/themes/kingsguard/admin/jobseeker-dashboard/single-jobs-content.php
@@ -1,6 +1,170 @@
-<div class="singleJob-content">
-    <h1><?php the_title(); ?></h1>
-    <div class="job-content">
-        <?php the_content(); ?>
+<div class="singleJobcontent">
+    <div class="singleJobTitle">
+        <div class="row">
+            <div class="col-md-9">
+                <h1><?php the_title(); ?></h1>
+            </div>
+            <div class="col-md-3">
+                <div class="applyBtn">
+                    <a href="javascript:void(0)" class="btn-style gradientBtn"> Apply Now </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="jobContent">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="jobContentInner">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="jobsInfo">
+                    <?php 
+                        $job_types = wp_get_post_terms(get_the_ID(), 'jobs_job_types', array('fields' => 'names'));
+                        $job_locations = wp_get_post_terms(get_the_ID(), 'jobs_job_locations', array('fields' => 'names'));
+                    ?>
+                    <div class="row">
+                        <?php if (!empty($job_types)) : ?>
+                            <div class="col-xl-4 col-md-6 col-sm-4">
+                                <div class="jobInfoInner">
+                                    <div class="jobInfoIcon">
+                                        <i class="fa fa-briefcase" aria-hidden="true"></i>
+                                    </div>
+                                    <div class="jobInfoText">
+                                        <h6>Job Type</h6>
+                                        <p><?php echo !empty($job_types) ? esc_html(implode(', ', $job_types)) : ''; ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endif; ?>
+                        <?php if (!empty($job_locations)) : ?>
+                            <div class="col-xl-4 col-md-6 col-sm-4">
+                                <div class="jobInfoInner">
+                                    <div class="jobInfoIcon">
+                                        <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                    </div>
+                                    <div class="jobInfoText">
+                                        <h6>Job Location</h6>
+                                        <p><?php echo !empty($job_locations) ? esc_html(implode(', ', $job_locations)) : ''; ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endif; ?>
+                        <div class="col-xl-4 col-md-6 col-sm-4">
+                            <div class="jobInfoInner">
+                                <div class="jobInfoIcon">
+                                    <i class="fa fa-calendar-check-o" aria-hidden="true"></i>
+                                </div>
+                                <div class="jobInfoText">
+                                    <h6>Job Posted</h6>
+                                    <p><?php echo get_the_date(); ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="jobsInfo">
+                    <div class="row">
+                        <?php
+                            $jobs_company = get_field('jobs_company');
+                            if ($jobs_company) {
+                        ?>
+                        <div class="col-xl-4 col-md-6 col-sm-4">
+                            <div class="jobInfoInner">
+                                <div class="jobInfoIcon">
+                                    <i class="fa fa-building" aria-hidden="true"></i>
+                                </div>
+                                <div class="jobInfoText">
+                                    <h6>Company</h6>
+                                    <p><?php echo esc_html($jobs_company); ?></p>
+                                </div>
+                            </div>
+                        </div>
+                        <?php } ?>
+                        <?php
+                            $job_expire_in = get_field('job_expire_in');
+                            if ($job_expire_in) {
+                        ?>
+                        <div class="col-xl-4 col-md-6 col-sm-4">
+                            <div class="jobInfoInner">
+                                <div class="jobInfoIcon">
+                                    <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                </div>
+                                <div class="jobInfoText">
+                                    <h6>Job Expire in</h6>
+                                    <p><?php echo esc_html($job_expire_in); ?></p>
+                                </div>
+                            </div>
+                        </div>
+                        <?php } ?>
+                        <?php
+                            $job_level = get_field('job_level');
+                            if ($job_level) {
+                        ?>
+                        <div class="col-xl-4 col-md-6 col-sm-4">
+                            <div class="jobInfoInner">
+                                <div class="jobInfoIcon">
+                                    <i class="fa fa-database" aria-hidden="true"></i>
+                                </div>
+                                <div class="jobInfoText">
+                                    <h6>Job Level</h6>
+                                    <p><?php echo esc_html($job_level); ?></p>
+                                </div>
+                            </div>
+                        </div>
+                        <?php } ?>
+                        <?php
+                            $experience = get_field('experience');
+                            if ($experience) {
+                        ?>
+                        <div class="col-xl-4 col-md-6 col-sm-4">
+                            <div class="jobInfoInner">
+                                <div class="jobInfoIcon">
+                                    <i class="fa fa-pie-chart" aria-hidden="true"></i>
+                                </div>
+                                <div class="jobInfoText">
+                                    <h6>Experience</h6>
+                                    <p><?php echo esc_html($experience); ?></p>
+                                </div>
+                            </div>
+                        </div>
+                        <?php } ?>
+                        <?php
+                            $jobs_education = get_field('jobs_education');
+                            if ($jobs_education) {
+                        ?>
+                        <div class="col-xl-4 col-md-6 col-sm-4">
+                            <div class="jobInfoInner">
+                                <div class="jobInfoIcon">
+                                    <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                                </div>
+                                <div class="jobInfoText">
+                                    <h6>Education</h6>
+                                    <p><?php echo esc_html($jobs_education); ?></p>
+                                </div>
+                            </div>
+                        </div>
+                        <?php } ?>
+                        <?php
+                            $jobs_salary = get_field('jobs_salary');
+                            if ($jobs_salary) {
+                        ?>
+                        <div class="col-xl-4 col-md-6 col-sm-4">
+                            <div class="jobInfoInner">
+                                <div class="jobInfoIcon">
+                                    <i class="fa fa-credit-card-alt" aria-hidden="true"></i>
+                                </div>
+                                <div class="jobInfoText">
+                                    <h6>Salary</h6>
+                                    <p><?php echo esc_html($jobs_salary); ?></p>
+                                </div>
+                            </div>
+                        </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
--- wp-content/themes/kingsguard/admin/jobseeker-register.php
+++ wp-content/themes/kingsguard/admin/jobseeker-register.php
@@ -12,8 +12,30 @@ get_header();
 
 ?> 
 
-<div class="register-wrapper">
-     <?php echo do_shortcode('[jobseekers_register_form]') ?>
+<div class="registerWrapper">
+    <div class="sm_container">
+        <div class="registerInner">
+            <?php
+                $kingsguard_logo_img = get_field('kingsguard_logo_img');
+                if (isset($kingsguard_logo_img) && !empty($kingsguard_logo_img)) {
+                    $image_url = wp_get_attachment_image_src($kingsguard_logo_img, 'full')[0];
+                    $image_alt = get_post_meta($kingsguard_logo_img, '_wp_attachment_image_alt', true);
+            ?>
+            <div class="kingsLogo">
+                <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($image_alt); ?>">
+            </div>
+            <?php } ?>
+            <?php
+                $page_title = get_field('page_title');
+                if ($page_title) {
+            ?>
+            <div class="jobRegisterTitle">
+                <h2 class="h2"><?php echo esc_html($page_title); ?></h2>
+            </div>
+            <?php } ?>
+            <?php echo do_shortcode('[jobseekers_register_form]') ?>
+        </div>
+    </div>
 </div>
 
 <?php get_footer(); ?>  
