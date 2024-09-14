57e2b863687de046fa48784b51b4b4ed33865745
sb62dev <sb62dev@gmail.com>
2024-09-14 20:04:32 +0530

    Updations
    Updations

---
+++ devstg/wp-content/themes/kingsguard/templates/quote.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Template Name: Quote Page
+**/
+get_header();
+
+?>
+
+<div class="pageBody quotePageBody">
+    <section class="quoteWrapper pb50">
+        <div class="sm_container">
+            <div class="quoteInnerWrap">
+                <div class="row">
+                    <div class="col-md-3">
+                        <?php
+                            $banner_title = get_field('quotation_banner_title');
+                            if (isset($banner_title) && !empty($banner_title)) {
+                            $banner_description = get_field('quotation_banner_description');
+                        ?>
+                        <section>
+                            <div class="commonBannerWrapper quoteWrapper">
+                                <div class="commonBanner" data-aos="fade-down" data-aos-duration="1000">
+                                    <div class="bannerContent">
+                                        <h1 class="mb0 h2"> <?php echo $banner_title; ?> </h1>
+                                        <div class="banner_cont"><?php echo $banner_description; ?></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <?php } ?>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="quoteInnerWrapper">
+                            <?php echo do_shortcode('[contact_form]'); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> 
+</div>
+
+<?php get_footer(); ?>
